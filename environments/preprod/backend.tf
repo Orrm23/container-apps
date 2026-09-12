5fc43cc144fa043d9bfb3e18e1c37f95bb967f04
Burhan yusuf <burhanusuf7@gmail.com>
2026-09-13 00:05:07 +0530

    added backend config

--- environments/preprod/backend.tf
+++ environments/preprod/backend.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "pipeline"
+    storage_account_name = "pipelinestorage"
+    container_name = "containerpipeline"
+    key = "terraform.tfstate"
+    
+  }
 }
 
 provider "azurerm" {
